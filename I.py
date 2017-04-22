a9db9e6137fe95c1bde3cfddc899d628e4680ff5
Daniel Share <danielandrewshare@gmail.com>
2017-04-22 14:51:18 +0100

    Added debug strings

--- I.py
+++ I.py
@@ -352,6 +352,7 @@ class I:
                 self.uuid_to_chat(uuid).profile_picture_location = ppl
             if users is not None:
                 self.uuid_to_chat(uuid).update_users(users)
+                print "Received update to users from " + address + " (" + str(users) + ")"
             if banned is not None:
                 self.uuid_to_chat(uuid).update_banned(banned)
 
